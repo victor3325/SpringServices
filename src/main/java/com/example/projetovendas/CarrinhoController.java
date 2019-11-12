f761af0ba21b0e1dd2f665ef71bb247ea02d2ba6
Your Name <you@example.com>
2019-11-12 20:52:27 -0200

    Carrinho Controller Finalizado

--- src/main/java/com/example/projetovendas/CarrinhoController.java
+++ src/main/java/com/example/projetovendas/CarrinhoController.java
@@ -6,14 +6,12 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 
-public class CarrinhoController {
-
 @Controller
-final class ProductController {
+final class CarrinhoController {
 
 	private final CarrinhoRepository carrinhoRepository;
 
-	ProductController(final CarrinhoRepository carrinhoRepository) {
+	CarrinhoController(final CarrinhoRepository carrinhoRepository) {
 		this.carrinhoRepository = carrinhoRepository;
 	}
 
@@ -22,76 +20,71 @@ final class ProductController {
 		carrinhoEntity.setValor_unitario(carrinho.getValor_unitario());
 		carrinhoEntity.setValor_frete(carrinho.getvalor_Frete());
 		carrinhoEntity.setTotal_pedido(carrinho.getTotal_pedido());
-		
-		
 	}
-/*
+
 	private static CarrinhoEntity toEntity(final Carrinho carrinho) {
-	
-		return new carrinhoEntity();
+		final int id = carrinho.getId();
+		final double valor_unitario = carrinho.getValor_unitario();
+		final double valor_frete = carrinho.getvalor_Frete();
+		final double total_pedido = carrinho.getTotal_pedido();
+		return new CarrinhoEntity(id,valor_unitario, valor_frete, total_pedido);
 	}
 
-	private static carrinho toDTO(final CarrinhoEntity carrinhoEntity) {
+	private static Carrinho toDTO(final CarrinhoEntity carrinhoEntity) {
 
-		return new carrinho();
+		final int id = carrinhoEntity.getId();
+		final double valor_unitario = carrinhoEntity.getValor_unitario();
+		final double valor_frete = carrinhoEntity.getValor_frete();
+		final double total_pedido = carrinhoEntity.getTotal_pedido();
+		return new Carrinho(id,valor_unitario, valor_frete, total_pedido);
 	}
 
 	private boolean isNotExistsProductByIdentifier(final Long id) {
 		// TODO Auto-generated method stub
-		return !this.CarrinhoRepository.existsById(id);
+		return !this.carrinhoRepository.existsById(id);
 	}
 
-	List<carrinho> getAllProducts() {
-		final List<carrinho> products = new ArrayList<>();
-		this.CarrinhoRepository.findAll().forEach(carrinhoEntity -> products.add(ProductController.toDTO(carrinhoEntity)));
-		/*
-		 * final Iterable<carrinhoEntity> entities = this.CarrinhoRepository.findAll();
-		 * for (final carrinhoEntity carrinhoEntity : entities) {
-		 * products.add(ProductController.toDTO(carrinhoEntity)); }
-		 */
-		/*
-		 * for (final Iterator<carrinhoEntity> iterator = entities.iterator();
-		 * iterator.hasNext();) { final carrinhoEntity carrinhoEntity = iterator.next();
-		 * products.add(ProductController.toDTO(carrinhoEntity)); }
-		 
-		return products;
+	List<Carrinho> getAllCarrinho() {
+		final List<Carrinho> carrinho = new ArrayList<>();
+		this.carrinhoRepository.findAll().forEach(carrinhoEntity -> carrinho.add(CarrinhoController.toDTO(carrinhoEntity)));
+		return carrinho;
 	}
 
-	carrinho getProduct(final Long id) {
-		final Optional<carrinhoEntity> optionalProduct = this.CarrinhoRepository.findById(id);
-		if (optionalProduct.isPresent()) {
-			return ProductController.toDTO(optionalProduct.get());
+	Carrinho get(final Long id) {
+		final Optional<CarrinhoEntity> optionalCarrinho = this.carrinhoRepository.findById(id);
+		if (optionalCarrinho.isPresent()) {
+			return CarrinhoController.toDTO(optionalCarrinho.get());
 		}
-		return carrinho.NULL_VALUE;
+		return Carrinho.NULL_VALUE;
 	}
 
-	carrinho removeProduct(final Long id) {
-		final Optional<carrinhoEntity> optionalProduct = this.CarrinhoRepository.findById(id);
-		if (optionalProduct.isPresent()) {
-			final carrinhoEntity carrinhoEntity = optionalProduct.get();
-			this.CarrinhoRepository.delete(carrinhoEntity);
-			return ProductController.toDTO(carrinhoEntity);
+	Carrinho removeCarrinho(final Long id) {
+		final Optional<CarrinhoEntity> optionalCarrinho = this.carrinhoRepository.findById(id);
+		if (optionalCarrinho.isPresent()) {
+			final CarrinhoEntity carrinhoEntity = optionalCarrinho.get();
+			this.carrinhoRepository.delete(carrinhoEntity);
+			return CarrinhoController.toDTO(carrinhoEntity);
 		}
-		return carrinho.NULL_VALUE;
+		return Carrinho.NULL_VALUE;
 	}
 
-	Long insertProduct(final carrinho carrinho) {
-		final carrinhoEntity carrinhoEntity = ProductController.toEntity(carrinho);
-		this.CarrinhoRepository.save(carrinhoEntity);
+	int insertCarrinho(final Carrinho carrinho) {
+		final CarrinhoEntity carrinhoEntity = CarrinhoController.toEntity(carrinho);
+		this.carrinhoRepository.save(carrinhoEntity);
 		return carrinhoEntity.getId();
 	}
 
-	carrinho updateProduct(final Long id, final carrinho carrinho) {
-		final Optional<carrinhoEntity> optionalProduct = this.CarrinhoRepository.findById(id);
-		if (optionalProduct.isPresent()) {
-			final carrinhoEntity carrinhoEntity = optionalProduct.get();
-			final carrinho oldcarrinho = ProductController.toDTO(carrinhoEntity);
-			ProductController.updateEntityFromDTO(carrinho, carrinhoEntity);
-			this.CarrinhoRepository.save(carrinhoEntity);
+	Carrinho updateProduct(final Long id, final Carrinho carrinho) {
+		final Optional<CarrinhoEntity> optionalCarrinho = this.carrinhoRepository.findById(id);
+		if (optionalCarrinho.isPresent()) {
+			final CarrinhoEntity carrinhoEntity = optionalCarrinho.get();
+			final Carrinho oldcarrinho = CarrinhoController.toDTO(carrinhoEntity);
+			CarrinhoController.updateEntityFromDTO(carrinho, carrinhoEntity);
+			this.carrinhoRepository.save(carrinhoEntity);
 			return oldcarrinho;
 		}
 		return carrinho.NULL_VALUE;
-		*/
+		
 	}
 
 }
